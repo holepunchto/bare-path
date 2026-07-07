9e7faa6f177aee5f4f6ddfc64c94d89b27ec0f75
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-07-07 18:49:15 +0200

    Validate path components (#7)

--- test.js
+++ test.js
@@ -75,3 +75,42 @@ test('resolve of a relative path uses the current working directory', (t) => {
 
   t.ok(path.isAbsolute(resolved))
 })
+
+test('join throws on non-string components', (t) => {
+  for (const value of [0, 1, false, true, null, undefined, {}, [], NaN]) {
+    t.exception.all(
+      () => path.posix.join('foo', value, 'bar'),
+      /must be of type string/,
+      `posix ${typeof value} (${String(value)})`
+    )
+    t.exception.all(
+      () => path.win32.join('foo', value, 'bar'),
+      /must be of type string/,
+      `win32 ${typeof value} (${String(value)})`
+    )
+  }
+})
+
+test('join still skips empty string components', (t) => {
+  t.is(path.posix.join('foo', '', 'bar'), 'foo/bar')
+  t.is(path.win32.join('foo', '', 'bar'), 'foo\\bar')
+})
+
+test('path APIs throw on non-string arguments', (t) => {
+  for (const impl of [path.posix, path.win32]) {
+    t.exception.all(() => impl.normalize(42), /must be of type string/)
+    t.exception.all(() => impl.isAbsolute(42), /must be of type string/)
+    t.exception.all(() => impl.dirname(42), /must be of type string/)
+    t.exception.all(() => impl.basename(42), /must be of type string/)
+    t.exception.all(() => impl.extname(42), /must be of type string/)
+    t.exception.all(() => impl.resolve('foo', 42), /must be of type string/)
+    t.exception.all(() => impl.relative('foo', 42), /must be of type string/)
+    t.exception.all(() => impl.relative(42, 'foo'), /must be of type string/)
+    t.exception.all(() => impl.basename('foo', 42), /must be of type string/)
+  }
+})
+
+test('toNamespacedPath returns non-strings unchanged', (t) => {
+  t.is(path.win32.toNamespacedPath(42), 42)
+  t.is(path.posix.toNamespacedPath(42), 42)
+})
